451911a2cf16298b206df93fc2aeadd5097f10ba
Pedr0-4raujo <pedroauraujf@gmail.com>
2026-09-04 17:19:39 -0300

    Adiciona projeto concluido

---
+++ MediaDeNotas/src/br/com/controle/CalcularMedia.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author Pedro
+ */
+public class CalcularMedia {
+        double valor1;
+    double valor2;
+    double somaDasNotas;
+private double mediaDasNotas;
+private int quantidadeDeAlunos;
+private double mediaDeAlunos;
+    
+}
